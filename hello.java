18eecf9d58001936b3b7adfe4aa8884aa65a610a
swaramaitre <swaracodes@gmail.com>
2024-10-28 16:46:34 +0530

    added two numbers

--- hello.java
+++ hello.java
@@ -1,9 +1,18 @@
+import java.util.*;
 
 public class hello
 {
     public static void main(String args[])
     {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("hello world");
+        System.out.println("Enter a number ");
+        int a = sc.nextInt();
+        System.out.println("enter second number ");
+        int b = sc.nextInt();
+        System.out.println("Sum of two number is " + (a+b));
+
     }
 
 }
