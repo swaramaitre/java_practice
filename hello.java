04b6ea48a529a179c87374ebd66bc0fa9b1b26cd
swaramaitre <swaracodes@gmail.com>
2024-10-29 09:20:47 +0530

    new file added hello world changed

--- hello.java
+++ hello.java
@@ -1,18 +1,9 @@
-import java.util.*;
 
 public class hello
 {
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-
-        // System.out.println("hello world");
-        System.out.println("Enter a number ");
-        int a = sc.nextInt();
-        System.out.println("enter second number ");
-        int b = sc.nextInt();
-        System.out.println("Sum of two number is " + (a+b));
-
+      System.out.println("hello world");  
     }
 
 }
---
+++ sum.java
@@ -0,0 +1,13 @@
+
+public class sum 
+{
+
+    public static void main(String args[])
+    {
+      int num1 = 6;
+      int num2 = 5;
+      int num3 = 3;
+      int sum = num1 + num2 + num3;
+      System.out.println(sum);
+    }
+}
