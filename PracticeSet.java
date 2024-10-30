4b50fb9adf4fc5ba07811a464201a1c66faf4862
swaramaitre <swaracodes@gmail.com>
2024-10-30 11:07:55 +0530

    solved practice problems

---
+++ PracticeSet.java
@@ -0,0 +1,19 @@
+
+public class PracticeSet {
+    public static void main(String[] args) {
+        // Question 1 sum of three numbers
+        int a = 4;
+        int b = 7;
+        int c = 6;
+        int sum = a + b + c;
+        System.out.println(sum);
+
+        // Question 2 CGPA of three subjects(out of 100)
+        float subject1 = 45;
+        float subject2 = 95;
+        float subject3 = 48;
+        float cgpa = (subject1 + subject2 +subject3)/30;
+        System.out.println(cgpa);
+
+    }
+}
