9dd9914b648f63a747e11ab69fc4edc4546d29c8
swaramaitre <swaracodes@gmail.com>
2024-10-30 12:12:27 +0530

    practiced operators

---
+++ operators.java
@@ -0,0 +1,28 @@
+public class operators {
+    public static void main(String[] args) {
+       int a = 4;
+       int b = 6 + a;
+       System.out.println(b); 
+
+       int s = 4;
+       int p = 6 - a;
+       System.out.println(p);
+
+       int q = 4;
+       int t = 6 * a;
+       System.out.println(t);   
+
+       float x = 4;
+       float y = 6 / x;
+       System.out.println(y);
+
+       int u = 4;
+       int v = 6 % u; // Modulo operator
+       System.out.println(v);
+
+       System.out.println(6==8); // comparison operator
+
+       System.out.println(64>5 && 64<8);
+       System.out.println(64>5 || 64>98); // Logical operator 
+    } 
+}
