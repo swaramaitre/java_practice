ab3356e5460d1d27415a95e6c1aa8dd3243d4f4d
swaramaitre <swaracodes@gmail.com>
2024-11-07 22:57:02 +0530

    strings in java

---
+++ String.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+    // String name = new String(original:"Swara")
+    // String name = "Swara";
+    //System.out.println("The name is: ");
+    //System.out.println(name);
+    //int a = 6;
+    //float b = 5.6454f;
+    //System.out.println("THe value of a is %d and b is %f", a, b);
+    //System.out.format("THe value of a is %d and b is %f", a, b);
+    Scanner sc = new Scanner (System.in);
+    //String st = sc.next();
+    String st = sc.nextLine();
+    System.out.println(st);
+
+    }
+}
