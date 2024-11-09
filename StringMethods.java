c99d308caece8b257f03f1a726c10c88cf7db2af
swaramaitre <swaracodes@gmail.com>
2024-11-09 21:29:58 +0530

    different methods of string

--- StringMethods.java
+++ StringMethods.java
@@ -1,3 +1,20 @@
 public class StringMethods {
-    
+    public static void main(String[] args) {
+        String name = "Harry";
+        //System.out.println(name);
+        int value = name.length();
+        System.out.println(value);
+
+        String lstring = name.toLowerCase();
+        System.out.println(lstring);
+
+        String ustring = name.toupperCase();
+        System.out.println(ustring);
+
+        String nonTrimmedString = "   Harry   ";
+        System.out.println(nonTrimmedString);
+
+        String trimmedString = nonTrimmedString.trim();
+        System.out.println(trimmedString.trim());
+    }
 }
